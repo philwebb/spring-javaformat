eec8524899d5b8629a8d8b1796fee7d4b3aecddd
Phillip Webb <pwebb@vmware.com>
2023-02-09 13:24:19 -0800

    Hack

--- spring-javaformat-vscode/spring-javaformat-vscode-extension/src/main/typescript/extension.ts
+++ spring-javaformat-vscode/spring-javaformat-vscode-extension/src/main/typescript/extension.ts
@@ -1,11 +1,11 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "mrphil" is now active!');
-	let disposable = vscode.commands.registerCommand('mrphil.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from TEST!');
-	});
-	context.subscriptions.push(disposable);
+  console.log('Congratulations, your extension "mrphil" is now active!')
+  let disposable = vscode.commands.registerCommand('mrphil.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World from TEST!')
+  })
+  context.subscriptions.push(disposable)
 }
 
 export function deactivate() {}
