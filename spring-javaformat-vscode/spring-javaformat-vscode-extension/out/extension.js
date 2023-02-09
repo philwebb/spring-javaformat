68bbb85918eed4d9577999efa396af47e841f839
Phillip Webb <pwebb@vmware.com>
2023-02-09 13:43:03 -0800

    Hacking

--- spring-javaformat-vscode/spring-javaformat-vscode-extension/out/extension.js
+++ spring-javaformat-vscode/spring-javaformat-vscode-extension/out/extension.js
@@ -1,15 +1,15 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.deactivate = exports.activate = void 0
-const vscode = require('vscode')
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = require("vscode");
 function activate(context) {
-  console.log('Congratulations, your extension "mrphil" is now active!')
-  let disposable = vscode.commands.registerCommand('mrphil.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from TEST!')
-  })
-  context.subscriptions.push(disposable)
+    console.log('Congratulations, your extension "mrphil" is now active!');
+    let disposable = vscode.commands.registerCommand('mrphil.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from TEST!');
+    });
+    context.subscriptions.push(disposable);
 }
-exports.activate = activate
-function deactivate() {}
-exports.deactivate = deactivate
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
