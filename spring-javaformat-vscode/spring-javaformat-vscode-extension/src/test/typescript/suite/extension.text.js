780e0de5a2992c822d12bea59310f5093ec3b942
Phillip Webb <pwebb@vmware.com>
2023-02-13 21:31:07 -0800

    Hack

--- spring-javaformat-vscode/spring-javaformat-vscode-extension/src/test/typescript/suite/extension.text.js
+++ spring-javaformat-vscode/spring-javaformat-vscode-extension/src/test/typescript/suite/extension.text.js
@@ -1,12 +1,13 @@
-import * as assert from 'assert';
-
-import * as vscode from 'vscode';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = require("assert");
+const vscode = require("vscode");
 // import * as myExtension from '../../extension';
-
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-	test('Sample test', () => {
-		assert.strictEqual([1, 2, 3].indexOf(5), -1);
-		assert.strictEqual([1, 2, 3].indexOf(0), -1);
-	});
+    vscode.window.showInformationMessage('Start all tests.');
+    test('Sample test', () => {
+        assert.strictEqual([1, 2, 3].indexOf(5), -1);
+        assert.strictEqual([1, 2, 3].indexOf(0), -1);
+    });
 });
+//# sourceMappingURL=extension.text.js.map
