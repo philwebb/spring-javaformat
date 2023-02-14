402b02e6cb4a670598294d1706b80fbc3317fcfb
Phillip Webb <pwebb@vmware.com>
2023-02-14 12:39:51 -0800

    Hack

--- spring-javaformat-vscode/spring-javaformat-vscode-extension/src/test/typescript/suite/index.ts
+++ spring-javaformat-vscode/spring-javaformat-vscode-extension/src/test/typescript/suite/index.ts
@@ -1,33 +1,33 @@
-import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import * as path from 'path'
+import * as Mocha from 'mocha'
+import * as glob from 'glob'
 
-console.log("running222")
+console.log('running222')
 
 export function run(): Promise<void> {
-	const mocha = new Mocha({
-		ui: 'tdd'
-	});
-	const testsRoot = path.resolve(__dirname, '..');
-	return new Promise((resolve, reject) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			console.log(files)
-			if (err) {
-				return reject(err);
-			}
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-			try {
-				mocha.run(failures => {
-					if (failures > 0) {
-						reject(new Error(`${failures} tests failed.`));
-					} else {
-						resolve();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				reject(err);
-			}
-		});
-	});
+  const mocha = new Mocha({
+    ui: 'tdd',
+  })
+  const testsRoot = path.resolve(__dirname, '..')
+  return new Promise((resolve, reject) => {
+    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+      console.log(files)
+      if (err) {
+        return reject(err)
+      }
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)))
+      try {
+        mocha.run((failures) => {
+          if (failures > 0) {
+            reject(new Error(`${failures} tests failed.`))
+          } else {
+            resolve()
+          }
+        })
+      } catch (err) {
+        console.error(err)
+        reject(err)
+      }
+    })
+  })
 }
